8f8b3ec21db56b66e09fb3b262531c8f54eaaccb
Anthony Guiot <anthony.guiot@univ-lorraine.fr>
2016-10-13 10:37:13 +0200

    requetes ajax

--- Frontend_Istex_usage/js/laboratory.js
+++ Frontend_Istex_usage/js/laboratory.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
     $("button").click(function(){
-        $.post("http://localhost/Backend_Istex_usage/src/index.php/getcountrys",
+        $.post("http://localhost/Backend_Istex_usage/src/index.php/getlaboratorys",
         {
           query: '"environmental impact"AND"rare earth element"'
         },
         function(data){
             var parsed = JSON.parse(data);
 			var r = []
-			console.log(parsed);
+      console.log(parsed);
 	    	for (var k in parsed) {
 	        var occurence=(Object.keys(parsed[k]).length)-1;
 	        if (!parsed.hasOwnProperty(k)) 
 	            continue
-	        $( "#country" ).append('<div class="item"><div class="content"><div class="header">'+k+'('+occurence+')'+'</div> </div> </div>' );
+	        $( "#laboratorys" ).append('<div class="item"><div class="content"><div class="header">'+k+'('+occurence+')'+'</div> </div> </div>' );
    		 }
         });
    	});
