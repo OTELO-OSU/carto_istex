81cca5c1f101ae6627874af66909f529dab787ab
Anthony Guiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-07-05 14:34:35 +0200

    MOdification API nominatim

--- Nominatim_Docker/install_custom.sh
+++ Nominatim_Docker/install_custom.sh
@@ -3,23 +3,6 @@ curl -L $1 --create-dirs -o data.osm.pbf
 sudo -u nominatim ./utils/setup.php --osm-file data.osm.pbf --all --threads 2 
 
 
-
-
-
-
 sudo -u postgres createuser www-data
 
-tee /etc/apache2/conf-available/nominatim.conf << EOFAPACHECONF
-<Directory "/Nominatim-3.0.0/build/website">
-  Options FollowSymLinks MultiViews
-  AddType text/html   .php
-  DirectoryIndex search.php
-  Require all granted
-</Directory>
-
-Alias /nominatim /Nominatim-3.0.0/build/website
-EOFAPACHECONF
-
-a2enconf nominatim
-service apache2 start
 sudo -u nominatim -H ./utils/update.php --import-osmosis-all
--- Nominatim_Docker/install_planet.sh
+++ Nominatim_Docker/install_planet.sh
@@ -9,17 +9,5 @@ sudo -u nominatim -H ./utils/update.php --init-updates
 
 sudo -u postgres createuser www-data
 
-tee /etc/apache2/conf-available/nominatim.conf << EOFAPACHECONF
-<Directory "/Nominatim-3.0.0/build/website">
-  Options FollowSymLinks MultiViews
-  AddType text/html   .php
-  DirectoryIndex search.php
-  Require all granted
-</Directory>
 
-Alias /nominatim /Nominatim-3.0.0/build/website
-EOFAPACHECONF
-
-a2enconf nominatim
-service apache2 start
 sudo -u nominatim -H ./utils/update.php --import-osmosis-all
