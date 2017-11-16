068ef14061f57840d63a875f8ff62022e6bb15a4
anthonyguiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-11-16 09:20:17 +0100

    Shared buffers ENV var

--- Nominatim_Docker/entrypoint.sh
+++ Nominatim_Docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
- exec su postgres -c "/usr/lib/postgresql/9.5/bin/postgres  --config-file=/etc/postgresql/9.5/main/postgresql.conf" &
+sed  -i 's/^\(shared_buffers = \).*/\1'$SHARED_BUFFERS'/' /etc/postgresql/9.5/main/postgresql.conf 
+exec su postgres -c "/usr/lib/postgresql/9.5/bin/postgres  --config-file=/etc/postgresql/9.5/main/postgresql.conf" &
 chmod 777 /Nominatim-3.0.0/build/
 sleep 10
 php changepassword.php $POSTGRESQL_PASSWORD
