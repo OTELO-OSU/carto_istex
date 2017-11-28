d9fedd25bc68c34225df4029b98326ab0c818d1a
Anthonyguiot <guiot.anthony@free.fr>
2017-11-28 11:36:48 +0100

    Update entrypoint.sh

--- Cartoistex_Docker/entrypoint.sh
+++ Cartoistex_Docker/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 php Projet_carto_istex/Cartoistex_Docker/changepassword.php $POSTGRESQL_PASSWORD
 service apache2 start
-trap 'exit 0' SIGTERM
-while true; do :; done
+tail -f /dev/null
