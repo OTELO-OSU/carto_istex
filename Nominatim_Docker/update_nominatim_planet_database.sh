04a5366486f9d9599727d7907a925aea8c3655a0
Anthonyguiot <guiot.anthony@free.fr>
2017-11-28 12:00:54 +0100

    Update update_nominatim_planet_database.sh

--- Nominatim_Docker/update_nominatim_planet_database.sh
+++ Nominatim_Docker/update_nominatim_planet_database.sh
@@ -5,6 +5,6 @@ cp install_planet/local.php settings/local.php
 sudo -u nominatim -H pip install --user osmium
 
 
+sudo -u nominatim -H ./utils/update.php --init-updates
 
-
-exec sudo -u nominatim -H ./utils/update.php --import-osmosis-all
+sudo -u nominatim -H ./utils/update.php --import-osmosis-all
